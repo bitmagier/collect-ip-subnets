09479076ed9b64ca6fd091f824570892a16e83f1
bitmagier <bitmagier@mailbox.org>
2021-09-30 09:05:43 +0000

    extended syslog search range from 9 month to 24 months

--- bash/extract-ip-address-from-syslog-blocked.sh
+++ bash/extract-ip-address-from-syslog-blocked.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# go through last 9 months of journal and grep all blocked IPs
-sudo journalctl -x --since="-9months"|grep Blocking | awk '{ print $7 };'|sed -e 's/"//g'|sed -e 's/\/32//g'
+# go through last X months of journal and grep all blocked IPs
+sudo journalctl -x --since="-24months"|grep Blocking | awk '{ print $7 };'|sed -e 's/"//g'|sed -e 's/\/32//g'
