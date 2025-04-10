44510d7fe61b006e6dbf17c7ff31c4413a08d9b3
bitmagier <bitmagier@mailbox.org>
2025-04-10 21:04:44 +0200

    tuned journalctl query

--- supplementary/shell-scripts/extract-ip-address-from-syslog-blocked.sh
+++ supplementary/shell-scripts/extract-ip-address-from-syslog-blocked.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # go through last X months of journal and grep all blocked IPs
-sudo journalctl -x --since="-36months"|grep Blocking | awk '{ print $7 };'|sed -e 's/"//g'|sed -e 's/\/32//g'
+sudo journalctl -x --since="-36months" --unit sshguard --grep="Blocking" | awk '{ print $7 };'|sed -e 's/"//g'|sed -e 's/\/32//g'
